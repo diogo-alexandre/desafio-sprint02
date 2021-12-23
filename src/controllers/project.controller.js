c365c70eb15aebabfa42df524065ee08933b7663
Diogo Alexandre <diogo.silva_bols@compasso.com.br>
2021-12-23 15:13:04 -0300

    create project controllers

---
+++ src/controllers/project.controller.js
@@ -0,0 +1,23 @@
+class ProjectController {
+  async findAll (req, res) {
+
+  }
+
+  async findOne (req, res) {
+
+  }
+
+  async insert (req, res) {
+
+  }
+
+  async update (req, res) {
+
+  }
+
+  async remove (req, res) {
+
+  }
+}
+
+module.exports = new ProjectController()
