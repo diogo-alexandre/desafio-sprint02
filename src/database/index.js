034bdc6853d24f948baa9aee448d26f6f9f9b748
Diogo Alexandre <diogo.silva_bols@compasso.com.br>
2021-12-23 23:02:40 -0300

    init models with sequelize

--- src/database/index.js
+++ src/database/index.js
@@ -1,14 +1,21 @@
 const Sequelize = require('sequelize')
 const dbConfig = require('../config/database.config')
 
+const Project = require('../models/Project')
+const Task = require('../models/Task')
+
 class Database {
   constructor () {
     this.connection = new Sequelize(dbConfig)
-    this.loadModels().forEach(model => model.init(this.connection))
+    this.models = [Project, Task]
+    this.init()
   }
 
-  async loadModels () {
-    return []
+  async init () {
+    this.models.forEach(model => model.init(this.connection))
+
+    Project.hasMany(Task, { foreignKey: 'projectId', as: 'tasks' })
+    Task.belongsTo(Project, { foreignKey: 'projectId', as: 'tasks' })
   }
 }
 
