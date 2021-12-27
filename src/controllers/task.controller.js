cae1a041dbba6f09c7712cc63fc3ed9a0c516d07
Diogo Alexandre <diogo.silva_bols@compasso.com.br>
2021-12-27 07:47:08 -0300

    add task controller

---
+++ src/controllers/task.controller.js
@@ -0,0 +1,24 @@
+
+class TaskController {
+  async findAll (req, res, next) {
+
+  }
+
+  async findOne (req, res, next) {
+
+  }
+
+  async insert (req, res, next) {
+
+  }
+
+  async remove (req, res, next) {
+
+  }
+
+  async head (req, res, next) {
+
+  }
+}
+
+module.exports = new TaskController()
