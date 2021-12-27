1c1c64e28d77bba00d096a0205b38c200bd69f8a
Diogo Alexandre <diogo.silva_bols@compasso.com.br>
2021-12-27 08:33:52 -0300

    add error handdle

--- src/app.js
+++ src/app.js
@@ -4,13 +4,13 @@ const bodyParser = require('body-parser')
 require('./database')
 const projectRouter = require('./routes/project.routes')
 const taskRouter = require('./routes/task.routes')
+const HttpError = require('./errors/http.error')
 
 class App {
   constructor () {
     this.express = express()
 
     this.middlewares()
-    this.routes()
   }
 
   middlewares () {
@@ -20,6 +20,16 @@ class App {
     this.express.use((req, res, next) => {
       res.set('Acess-Control-Allow-Origin', '*')
       next()
+    })
+
+    this.routes()
+
+    this.express.use((err, req, res, next) => {
+      if (err instanceof HttpError) {
+        res.status(err.statusCode).send(err.message)
+      } else {
+        res.status(500).send('Internal server error')
+      }
     })
   }
 
