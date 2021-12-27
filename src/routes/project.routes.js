0c58db4b4fdbcbe7b0c5c03f3ddc9ce84e312411
Diogo Alexandre <diogo.silva_bols@compasso.com.br>
2021-12-27 07:43:46 -0300

    add 'options' and 'head' routes to project.controller

--- src/routes/project.routes.js
+++ src/routes/project.routes.js
@@ -1,10 +1,22 @@
 const router = require('express').Router()
 const projectController = require('../controllers/project.controller')
 
+router.options('/project', (req, res) => {
+  res.set('Acess-Control-Allow-Methods', 'GET, POST')
+  res.status(201).end()
+})
+
 router.get('/project', projectController.findAll)
-router.get('/project/:id', projectController.findOne)
 router.post('/project', projectController.insert)
+
+router.options('/project/:id', (req, res) => {
+  res.set('Acess-Control-Allow-Methods', 'GET, PUT, DELETE')
+  res.status(201).end()
+})
+
+router.get('/project/:id', projectController.findOne)
 router.put('/project/:id', projectController.update)
 router.delete('/project/:id', projectController.remove)
+router.head('/project/:id', projectController.head)
 
 module.exports = router
